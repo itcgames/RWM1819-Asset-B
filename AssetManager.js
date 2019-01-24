cd7abfa2868b2abd8c5671f96ded8ccfafa4430f
C00206724 <c00206724@itcarlow.ie>
2019-01-24 19:24:54 +0000

    loading rotated image

--- AssetManager.js
+++ AssetManager.js
@@ -29,8 +29,30 @@ class  AssetManager{
         this.loop = true;
         this.flipped = false;
         this.horizontalSheet = true;
+        var TO_RADIANS = Math.PI/180;
     }
 
+
+    drawRotatedImage(image, x, y, angle, ctx)
+    {
+        // save the current co-ordinate system
+        // before we screw with it
+        ctx.save();
+
+        // move to the middle of where we want to draw our image
+        ctx.translate(x, y);
+
+        // rotate around that point, converting our
+        // angle from degrees to radians
+        ctx.rotate(angle * this.TO_RADIANS);
+
+        // draw it up and to the left by half the width
+        // and height of the image
+        ctx.drawImage(image, -(image.width/2), -(image.height/2));
+
+        // and restore the co-ords to how they were when we began
+        ctx.restore();
+    }
     /**
      * Draw function for the Image.
      */
